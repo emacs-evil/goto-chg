81fca94d2592cf32bab451c39a9cd2d9b03fbd6b
Vasilij Schneidermann <mail@vasilij.de>
2017-09-17 21:00:46 +0200

    Add maintainer line

--- goto-chg.el
+++ goto-chg.el
@@ -21,6 +21,7 @@
 ;;-------------------------------------------------------------------
 ;;
 ;; Author: David Andersson <l.david.andersson(at)sverige.nu>
+;; Maintainer: Vasilij Schneidermann <v.schneidermann@github.com>
 ;; Created: 16 May 2002
 ;; Version: 1.7
 ;; Keywords: convenience, matching
